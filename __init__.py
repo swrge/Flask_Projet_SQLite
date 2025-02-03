e58aa62be91ab7c086883c12d226c27139491199
Bad Random <149905301+swrge@users.noreply.github.com>
2025-02-03 16:37:16 +0000

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -1,4 +1,8 @@
-from flask import Flask, render_template, request, redirect, url_for, flash
+from flask import Flask, render_template_string, render_template, jsonify, request, redirect, url_for, session
+from flask import render_template
+from flask import json
+from urllib.request import urlopen
+from werkzeug.utils import secure_filename
 import sqlite3
 
 app = Flask(__name__)
